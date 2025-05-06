18d69bd675b278834d46a5c52ea9ecbe9a4e7373
RogLag <hugo.rog@icloud.com>
2025-05-06 21:25:19 +0200

    ✨ feat: integrate axios for data fetching in Greet.svelte

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,10 +1,21 @@
 <script>
-  import { invoke } from '@tauri-apps/api/tauri'
+  import axios from 'axios';
   import Button from './shadcn/components/ui/button/button.svelte';
   import Input from './shadcn/components/ui/input/input.svelte';
 
   let name = $state('');
   let greetMsg = $state('');
+
+  let fetching = $state({});
+
+  // example of axios usage
+  axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      fetching = response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
 
   function greet() {
     greetMsg = name
@@ -15,4 +26,5 @@
   <Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
   <Button variant="default" class="mt-4" onclick={greet}>Greet</Button>
   <p>{greetMsg}</p>
+  <p>Fetching data: {JSON.stringify(fetching)}</p>
 </div>
