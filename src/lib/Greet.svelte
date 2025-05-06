5a3f9c4edbd2c2ba40926382a8a1cee45465718c
RogLag <hugo.rog@icloud.com>
2025-05-06 21:03:05 +0200

    🎉 Initial commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { invoke } from '@tauri-apps/api/tauri'
+  import Button from './shadcn/components/ui/button/button.svelte';
+  import Input from './shadcn/components/ui/input/input.svelte';
+
+  let name = $state('');
+  let greetMsg = $state('');
+
+  async function greet() {
+    greetMsg = await invoke('greet', { name })
+  }
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen gap-4">
+  <Input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+  <Button variant="default" class="mt-4" onclick={greet}>Greet</Button>
+  <p>{greetMsg}</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Greet from "../lib/Greet.svelte";
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+  <Greet />
+</div>
